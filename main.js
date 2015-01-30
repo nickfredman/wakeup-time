dd667f13fcae97b2bc92d0176f37518669a4bf00
Nick Fredmangit config --global ntfredman@gmail.comgit config --global user.name Nick <ntfredman@gmail.com>
2015-01-29 17:34:05 -0700

    Clock starting to come together

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 $(document).on('ready', function() {
-  
+    var outerShell = $('<div class="outer-shell"></div>');
+    var innerShell = $('<div class="inner-shell"><p>PM</p><p><AUTO</p></div>');
+    var leftLabels = $('<div class="left-labels"></div>');
+    var clockScreen = $('<div class="clock-screen"></div>');
+    var clockIndicator = $('<div class="clock-indicator"></div>');
+    var clockText = $('<div class="clock-text"><span>12:00<span></div>');
+    var bottomAM = $('<div class="bottom-am"></div>');
+    var bottomFM = $('<div class="bottom-fm"></div>');
+
+    $('.container').append(outerShell);
+    outerShell.append(innerShell);
+    innerShell.append(leftLabels)
+        .append(clockScreen)
+        .append(bottomAM)
+        .append(bottomFM);
+    clockScreen.append(clockIndicator)
+        .append(clockText);
+
 });
